30e76a5579bdd3a01e2fe7396af1680a992a614d
Yazz AKM <sigmahack1337@gmail.com>
2025-11-11 21:08:45 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('downloadFree').addEventListener('click', function() {
+    window.open('https://files.catbox.moe/hhp1ww.zip');
+});
+
+document.getElementById('purchasePremium').addEventListener('click', function() {
+    window.open('https://discord.gg/dopamina', '_blank');
+});
+
